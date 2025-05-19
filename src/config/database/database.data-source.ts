790ace29a69a6353c98ed2d816bac54d7abcce47
Murad Imanov <imanovmurad896@gmail.com>
2025-05-19 19:02:43 +0300

    Refine database data source comment for clarity
    Add a comment clarifying that the provided data source is exclusively for database migrations and not for use in application code. This prevents potential misuse and improves code understanding for future developers.

--- src/config/database/database.data-source.ts
+++ src/config/database/database.data-source.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '../../app.module';
 
+// This data source is only used for database migrations
+// and should not be used in the application code
 export const dataSource = NestFactory.create(AppModule)
   .then((app) => app.get(DataSource))
   .then((dataSource) => Promise.all([dataSource, dataSource.destroy()]))
